946444aa3a7991e70e23892c8e5b65bfaa889b81
wideprism <wideprism@LUCERNEBORN>
2024-10-22 02:06:10 +0200

    Add project files.

---
+++ MnS_L1_GoL/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MnS_L1_GoL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            initDGV = new Button();
+            SuspendLayout();
+            // 
+            // initDGV
+            // 
+            initDGV.Location = new Point(602, 784);
+            initDGV.Name = "initDGV";
+            initDGV.Size = new Size(94, 29);
+            initDGV.TabIndex = 0;
+            initDGV.Text = "Start";
+            initDGV.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1204, 853);
+            Controls.Add(initDGV);
+            Name = "Form1";
+            Text = "Game of Life in DataGridView";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button initDGV;
+    }
+}
---
+++ MnS_L1_GoL/Form1.cs
@@ -0,0 +1,85 @@
+namespace MnS_L1_GoL
+{
+    public partial class Form1 : Form
+    {
+        static DataGridView dataGridView1 = new()
+        {
+            AutoGenerateColumns = false
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 50; i++)
+            {
+                dataGridView1.Columns.Add("col" + i, "Column " + i);
+                dataGridView1.Columns[i - 1].FillWeight = 1;
+            }
+            for (int j = 1; j <= 30; j++)
+            {
+                dataGridView1.Rows.Add();
+            }
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    cell.Value = 0;
+                }
+            }
+            dataGridView1.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowsDefaultCellStyle.BackColor = Color.White;
+            dataGridView1.RowsDefaultCellStyle.SelectionBackColor = Color.Silver;
+            dataGridView1.CellFormatting += new DataGridViewCellFormattingEventHandler(dataGridView1_CellFormatting);
+            dataGridView1.CellClick += new DataGridViewCellEventHandler(dataGridView1_CellClick);
+            this.AutoSize = true;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.Controls.Add(dataGridView1);
+        }
+
+        private void initDGV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Get the clicked cell
+                DataGridViewCell cell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+
+                if (cell.Value.ToString() == "0")
+                {
+                    cell.Value = 1;
+                }
+                else if (cell.Value.ToString() == "1")
+                {
+                    cell.Value = 0;
+                }
+            }
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            // Check that the current cell being formatted isn't in a header.
+            if (dataGridView1.Columns[e.ColumnIndex].Name != "ColumnName" && e.RowIndex >= 0)
+            {
+                if (e.Value.ToString() == "0")
+                {
+                    e.CellStyle.BackColor = Color.Red;
+                    e.CellStyle.SelectionBackColor = Color.LightSalmon;
+                }
+                else if (e.Value.ToString() == "1")
+                {
+                    e.CellStyle.BackColor = Color.Green;
+                    e.CellStyle.SelectionBackColor = Color.LightGreen;
+                }
+            }
+        }
+    }
+}
